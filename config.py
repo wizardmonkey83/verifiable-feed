2817f785d1d32037d40ec82946c74916c87af0e1
brompley <brompley@brompleys-mini.myfiosgateway.com>
2026-02-22 22:02:45 -0500

    working through buffer logic

---
+++ config.py
@@ -0,0 +1,7 @@
+from decouple import config
+
+PI_IP_ADDRESS = config("PI_IP_ADDRESS")
+
+DB_NAME = config("DB_NAME")
+PSQL_ADMIN_NAME = config("PSQL_ADMIN_NAME")
+PSQL_ADMIN_PASS = config("PSQL_ADMIN_PASS")
--- raspberry/camera.py
+++ raspberry/camera.py
@@ -1,7 +1,7 @@
 from config import PSQL_ADMIN_NAME, PSQL_ADMIN_PASS, PI_IP_ADDRESS, DB_NAME
 import psycopg2
 import cv2
-from time import sleep
+import time
 
 def build_cam_env():
     connection = psycopg2.connect(
@@ -15,17 +15,40 @@ def build_cam_env():
 
     try:
         cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_FPS, 15)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
         if not cap.isOpened():
             return Exception("Error: could not open video.")
-            exit()
 
-        cursor.execute("INSERT INTO recordings ")
+        time.sleep(2)
+
+        dequeue = []
+        while dequeue:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            if len(dequeue) >= 150:
+                dequeue.pop()
+            
+            dequeue.insert(0, frame)
+
+            
 
     except Exception as e:
         return Exception(f"Error during build_cam_env: {e}")
+    
+    finally:
+        if cap:
+            cap.release()
 
-    cursor.execute()
+        if cursor:
+            cursor.close()
+
+        if connection:
+            connection.close()
 
 if __name__ == "__main__":
     build_cam_env()
